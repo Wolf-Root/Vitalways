4356bc6c02889c69caa4d2b6f45eb169fb6ca220
Wolf-Roo <mobilemax363@gmail.com>
2025-09-16 21:34:38 +0100

    initial commit

---
+++ src/lib/gsam.ts
@@ -0,0 +1,8 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger);
+
+export { gsap, useGSAP, SplitText, ScrollTrigger };
